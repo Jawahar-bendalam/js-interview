10e689d9e04a314caa905e5130e6d5828accc367
jawahar.bendalam <jawahar.bendalam@tandemloop.in>
2024-11-19 10:32:09 +0530

    anagrams, copies and chunk programs added

--- interview.js
+++ interview.js
@@ -11,5 +11,45 @@ function flattenArray(arr) {
   return result;
 }
 
-let flattened = flattenArray([1, 2, [3, 4], 5, [6, [7, 8]]]);
-console.log(flattened);
+console.log(flattenArray([1, 2, [3, 4], 5, [6, [7, 8]]]));
+
+//Anagrams check
+function anagrams(str1, str2) {
+  return str1.split("").sort().join() == str2.split("").sort().join();
+}
+
+console.log(anagrams("listen", "silent"));
+
+//Shallow copy and deep copy
+let user = {
+  name: "john",
+  age: "28",
+  job: "SDE-1",
+};
+
+//Shallow copy
+let shallowUser = user;
+console.log("user", user);
+console.log("shallow user", shallowUser);
+shallowUser.name = "johndoe";
+console.log("user", user);
+console.log("shallow user", shallowUser);
+
+//Deep copy
+let deepUser = JSON.parse(JSON.stringify(user));
+console.log("user", user);
+console.log("deep user", deepUser);
+deepUser.name = "johndoe";
+console.log("user", user);
+console.log("deep user", deepUser);
+
+//string chunks
+function stringChunk(str, chunklength) {
+  let chunks = [];
+  for (let i = 0; i < str.length; i += chunklength) {
+    chunks.push(str.slice(i, i + chunklength));
+  }
+  return chunks;
+}
+
+console.log(stringChunk("jawahar", 2));
